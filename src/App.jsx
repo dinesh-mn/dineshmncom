7173781bd187ce431cb62e53d69340afee665296
Dinesh M <dineshmuthuswami@gmail.com>
2024-11-29 01:41:15 +0530

    Updated app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
+// eslint-disable-next-line no-unused-vars
 import React from "react"
 import Header from "./components/header/header";
 const App = () =>{
   return (
     <div>
-      <h1>HELLO S/h1>
+      <h1>HELLO S </h1>
       <Header/>
     </div>
   );
